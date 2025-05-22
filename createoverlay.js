0f73644d1e716203de2fbdbdfcad97c2d759443f
Andrew <155571246+ItsAsShrimpleAsThat@users.noreply.github.com>
2025-05-22 03:56:30 -0700

    Store reel ratings

--- createoverlay.js
+++ createoverlay.js
@@ -3,10 +3,34 @@ overlayElement.style = "position: fixed; top: 40%; right: 3%; width: 26%; height
 overlayElement.innerHTML = `<h2 style="text-align: center; justify-self: center; margin: 5px auto; font-size:large;">Reels Classifier</h2>
 <div style="width: 100%; display: flex; justify-content: center; height:30px">
     <p style="margin-right: 4px; margin-top: 2px;">Not Funny</p>
-    <input type="range" min="1" max="100" style="width: 60%" value="50" class="slider" id="myRange">
+    <input type="range" min="-100" max="100" style="width: 60%" value="0" class="slider" id="funnyslider">
     <p style="margin-left: 4px; margin-top: 2px;">Funny</p>
 </div>
 <div style="width: 100%; display: flex; justify-content: center; height:auto">
-    <p>Last Recorded: null</p>
+    <p id="lastrecordedvalue">Last Recorded: null</p>
 </div>`
 document.body.appendChild(overlayElement)   
+
+const funnySlider = document.getElementById("funnyslider");
+const lastrecordedvalue = document.getElementById("lastrecordedvalue");
+
+if(localStorage.getItem("reels") == null)
+{
+    localStorage.setItem("reels") = [];
+}
+
+const currentReelsData = localStorage.getItem("reels")
+
+funnySlider.addEventListener("change", function()
+{
+    let recordedValue = parseInt(funnySlider.value);
+    lastrecordedvalue.innerHTML = "Last Recorded: " + recordedValue;
+    
+    currentReelsData.push({
+        "reel_url": window.location.href,
+        "score": funnySlider.value,
+        "rating_timestamp": Date.now(),
+    })
+
+    funnySlider.value = 0; 
+});
